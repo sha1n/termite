3758d45beefc4051031b3da50201e42791cc42f0
sha1n <shai.nagar@gmail.com>
2021-05-28 13:28:20 +0300

    improved fake impl

--- fake_terminal.go
+++ fake_terminal.go
@@ -1,28 +1,34 @@
 package termite
 
 import (
+	"bufio"
 	"bytes"
-	"fmt"
-	"io"
-	"sync"
+	// "io"
 )
 
 type fakeTerm struct {
-	Out     *bytes.Buffer
-	Err     *bytes.Buffer
-	outLock *sync.Mutex
-	width   int
-	height  int
+	Terminal
+	Out    *bytes.Buffer
+	Err    *bytes.Buffer
+	width  int
+	height int
 }
 
 // NewFakeTerminal ...
 func NewFakeTerminal(width, height int) *fakeTerm {
+	outBuf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+
+	t := NewTerminal(true)
+	(t.(*term)).Out = bufio.NewWriter(outBuf)
+	(t.(*term)).Err = bufio.NewWriter(errBuf)
+
 	return &fakeTerm{
-		Out:     new(bytes.Buffer),
-		Err:     new(bytes.Buffer),
-		outLock: &sync.Mutex{},
-		width:   width,
-		height:  height,
+		Out:      outBuf,
+		Err:      errBuf,
+		width:    width,
+		height:   height,
+		Terminal: t,
 	}
 }
 
@@ -32,27 +38,4 @@ func (t *fakeTerm) Width() (width int) {
 
 func (t *fakeTerm) Height() (height int) {
 	return t.height
-}
-
-func (t *fakeTerm) StdOut() io.Writer {
-	return t.Out
-}
-
-func (t *fakeTerm) StdErr() io.Writer {
-	return t.Err
-}
-
-func (t *fakeTerm) Print(e interface{}) {
-	t.WriteString(fmt.Sprintf("%v", e))
-}
-
-func (t *fakeTerm) Println(e interface{}) {
-	t.WriteString(fmt.Sprintf("%v\r\n", e))
-}
-
-func (t *fakeTerm) WriteString(s string) (n int, err error) {
-	t.outLock.Lock()
-	defer t.outLock.Unlock()
-
-	return t.Out.WriteString(s)
 }
