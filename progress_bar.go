af157d2b250b14ef826efb420ab1649b8f43d0ba
Shai Nagar <sha1n@users.noreply.github.com>
2021-06-01 13:33:26 +0300

    added progress bar formatter api (#32)
    * added proggress bar formatter api
    
    * godoc

--- progress_bar.go
+++ progress_bar.go
@@ -9,6 +9,63 @@ import (
 	"sync"
 )
 
+const (
+	// DefaultProgressBarLeftBorder default progress bar left border character
+	DefaultProgressBarLeftBorder = '\u258F'
+
+	// DefaultProgressBarRightBorder default progress bar right border character
+	DefaultProgressBarRightBorder = '\u2595'
+
+	// DefaultProgressBarFill default progress bar fill character
+	DefaultProgressBarFill = '\u2587'
+)
+
+// DefaultProgressBarFormatter returns a new instance of the default ProgressBarFormatter
+func DefaultProgressBarFormatter() *SimpleProgressBarFormatter {
+	return &SimpleProgressBarFormatter{
+		LeftBorderChar:  DefaultProgressBarLeftBorder,
+		RightBorderChar: DefaultProgressBarRightBorder,
+		FillChar:        DefaultProgressBarFill,
+	}
+}
+
+// ProgressBarFormatter a formatter to control the style of a ProgressBar.
+type ProgressBarFormatter interface {
+	// FormatLeftBorder returns a string that contains one visible character and optionally
+	// additional styling charatcers such as color codes, background and other effects.
+	FormatLeftBorder() string
+
+	// FormatRightBorder returns a string that contains one visible character and optionally
+	// additional styling charatcers such as color codes, background and other effects.
+	FormatRightBorder() string
+
+	// FormatFill returns a string that contains one visible character and optionally
+	// additional styling charatcers such as color codes, background and other effects.
+	FormatFill() string
+}
+
+// SimpleProgressBarFormatter a simple ProgressBarFormatter implementation which is based on constructor values.
+type SimpleProgressBarFormatter struct {
+	LeftBorderChar  rune
+	RightBorderChar rune
+	FillChar        rune
+}
+
+// FormatLeftBorder returns the left border char
+func (f *SimpleProgressBarFormatter) FormatLeftBorder() string {
+	return fmt.Sprintf("%c", f.LeftBorderChar)
+}
+
+// FormatRightBorder returns the right border char
+func (f *SimpleProgressBarFormatter) FormatRightBorder() string {
+	return fmt.Sprintf("%c", f.RightBorderChar)
+}
+
+// FormatFill returns the fill char
+func (f *SimpleProgressBarFormatter) FormatFill() string {
+	return fmt.Sprintf("%c", f.FillChar)
+}
+
 // TickFn a tick handle
 type TickFn = func() bool
 
@@ -20,15 +77,13 @@ type ProgressBar interface {
 }
 
 type bar struct {
-	maxTicks    int
-	ticks       int
-	writer      io.Writer
-	width       int
-	leftBorder  string
-	rightBorder string
-	fill        string
-	active      bool
-	mx          *sync.RWMutex
+	maxTicks  int
+	ticks     int
+	writer    io.Writer
+	width     int
+	formatter ProgressBarFormatter
+	active    bool
+	mx        *sync.RWMutex
 }
 
 // NewProgressBar creates a new progress bar
@@ -36,26 +91,22 @@ type bar struct {
 // maxTicks 			- how many ticks are to be considered 100% of the progress
 // terminalWidth 	- the width of the terminal
 // width 					- bar width in characters
-// leftBorder 		- left border character
-// rightBorder 		- right border character
-// fill - fill character
-func NewProgressBar(writer io.Writer, maxTicks int, terminalWidth int, width int, leftBorder rune, rightBorder rune, fill rune) ProgressBar {
+// formatter 		  - a formatter for this progress bar
+func NewProgressBar(writer io.Writer, maxTicks int, terminalWidth int, width int, formatter ProgressBarFormatter) ProgressBar {
 	return &bar{
-		maxTicks:    maxTicks,
-		ticks:       0,
-		writer:      writer,
-		width:       min(width, terminalWidth-7), // 7 = 2 borders, 3 digits, % sign + 1 padding char
-		leftBorder:  string(leftBorder),
-		rightBorder: string(rightBorder),
-		fill:        string(fill),
-		mx:          &sync.RWMutex{},
+		maxTicks:  maxTicks,
+		ticks:     0,
+		writer:    writer,
+		width:     min(width, terminalWidth-7), // 7 = 2 borders, 3 digits, % sign + 1 padding char
+		formatter: formatter,
+		mx:        &sync.RWMutex{},
 	}
 }
 
 // NewDefaultProgressBar creates a progress bar with styling
 func NewDefaultProgressBar(writer io.Writer, terminalWidth int, maxTicks int) ProgressBar {
 	return NewProgressBar(
-		writer, maxTicks, terminalWidth/2, terminalWidth, '\u258F', '\u2595', '\u2587',
+		writer, maxTicks, terminalWidth/2, terminalWidth, DefaultProgressBarFormatter(),
 	)
 }
 
@@ -82,9 +133,10 @@ func (b *bar) Tick() bool {
 		fmt.Sprintf(
 			"%s%s%s%s%s %d%%\r",
 			TermControlEraseLine,
-			b.leftBorder, strings.Repeat(b.fill, charsToFill),
+			b.formatter.FormatLeftBorder(),
+			strings.Repeat(b.formatter.FormatFill(), charsToFill),
 			strings.Repeat(" ", spaceChars),
-			b.rightBorder,
+			b.formatter.FormatRightBorder(),
 			int(percent*100),
 		),
 	)
