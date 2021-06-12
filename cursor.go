aed9b41e7d96c50e6be6cdf952b84e0e19322ad4
Shai Nagar <shain@wix.com>
2021-06-12 11:44:21 +0300

    avoiding unnecessary pointer

--- cursor.go
+++ cursor.go
@@ -24,47 +24,47 @@ type cursor struct {
 
 // NewCursor returns a new cursor for the specified terminal
 func NewCursor(writer io.Writer) Cursor {
-	return &cursor{
+	return cursor{
 		writer: writer,
 	}
 }
 
-func (c *cursor) Position(row, col int) {
+func (c cursor) Position(row, col int) {
 	c.writeString(fmt.Sprintf("\033[%d;%dH", row, col))
 }
 
-func (c *cursor) Up(lines int) {
+func (c cursor) Up(lines int) {
 	c.writeString(fmt.Sprintf("\033[%dA", lines))
 }
 
-func (c *cursor) Down(lines int) {
+func (c cursor) Down(lines int) {
 	c.writeString(fmt.Sprintf("\033[%dB", lines))
 }
 
-func (c *cursor) Forward(cols int) {
+func (c cursor) Forward(cols int) {
 	c.writeString(fmt.Sprintf("\033[%dC", cols))
 }
 
-func (c *cursor) Backward(cols int) {
+func (c cursor) Backward(cols int) {
 	c.writeString(fmt.Sprintf("\033[%dD", cols))
 }
 
-func (c *cursor) Hide() {
+func (c cursor) Hide() {
 	c.writeString("\033[?25l")
 }
 
-func (c *cursor) Show() {
+func (c cursor) Show() {
 	c.writeString("\033[?25h")
 }
 
-func (c *cursor) SavePosition() {
+func (c cursor) SavePosition() {
 	c.writeString("\033[s")
 }
 
-func (c *cursor) RestorePosition() {
+func (c cursor) RestorePosition() {
 	c.writeString("\033[u")
 }
 
-func (c *cursor) writeString(s string) {
+func (c cursor) writeString(s string) {
 	io.WriteString(c.writer, s)
 }
