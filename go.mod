d65fd14ef7001f8501ca730f9fe49dc0797b8508
sha1n <sha1n@users.noreply.github.com>
2023-09-23 14:44:17 +0300

    added unlimited probed writer option

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/fatih/color v1.15.0
 	github.com/kopoli/go-terminal-size v0.0.0-20170219200355-5c97524c8b54
 	github.com/mattn/go-isatty v0.0.19
-	github.com/sha1n/gommons v0.0.14
+	github.com/sha1n/gommons v0.0.15
 	github.com/stretchr/testify v1.8.4
 )
 
--- thread_safe_buffer.go
+++ thread_safe_buffer.go
@@ -1,31 +1,31 @@
 package termite
 
 import (
-    "bytes"
-    "sync"
+	"bytes"
+	"sync"
 )
 
 type ThreadSafeBufferWriter struct {
-    buf    *bytes.Buffer
-    mutex  sync.Mutex
+	buf   *bytes.Buffer
+	mutex sync.Mutex
 }
 
 func NewThreadSafeBuffer() *ThreadSafeBufferWriter {
-    return &ThreadSafeBufferWriter{
-        buf: new(bytes.Buffer),
-    }
+	return &ThreadSafeBufferWriter{
+		buf: new(bytes.Buffer),
+	}
 }
 
 func (b *ThreadSafeBufferWriter) Write(p []byte) (n int, err error) {
-    b.mutex.Lock()
-    defer b.mutex.Unlock()
-    return b.buf.Write(p)
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+	return b.buf.Write(p)
 }
 
 func (b *ThreadSafeBufferWriter) String() string {
-    b.mutex.Lock()
-    defer b.mutex.Unlock()
-    return b.buf.String()
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+	return b.buf.String()
 }
 
 func (b *ThreadSafeBufferWriter) Len() int {
